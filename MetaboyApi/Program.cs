82ed36ef9ef2362d34c231733329dc3248e7053b
Fudgey <fudgebucket27@gmail.com>
2022-10-25 16:10:29 +1000

    cors

--- MetaboyApi/Program.cs
+++ MetaboyApi/Program.cs
@@ -7,10 +7,24 @@ namespace MetaboyApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
+
 
             builder.Services.AddControllers();
 
             var app = builder.Build();
+
+            app.UseCors("AllowAll");
 
             // Configure the HTTP request pipeline.
 
